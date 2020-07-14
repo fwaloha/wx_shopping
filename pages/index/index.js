1499277ad2fd0862a8aaab90ca030b1b03b287df
aloha <wangfei8646@126.com>
2020-07-15 00:09:04 +0800

    night

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,54 @@
 const app = getApp()
 
+import { request } from "../../request/index.js"
+
 Page({
   data: {
-    img_src: []
+    img_src: [],
+    catglist: []
   },
   onLoad: function () {
-    wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
 
-      success: (result) => {
-        console.log(result)
+    //   success: (result) => {
+    //     console.log(result)
 
+    //     this.setData({
+    //       img_src: result.data.message
+    //     })
+
+    //   },
+    //   fail: () => { },
+    //   complete: () => { }
+    // });
+
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+      .then(result => {
         this.setData({
           img_src: result.data.message
         })
+      })
+      
+    this.swipeFun();
+    this.catgFun();
 
-      },
-      fail: () => { },
-      complete: () => { }
-    });
 
+  },
+  swipeFun() {
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+      .then(result => {
+        this.setData({
+          img_src: result.data.message
+        })
+      })
+  },
+  catgFun() {
+    request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+      .then(result => {
+        this.setData({
+          catglist: result.data.message
+        })
+      })
   },
 })
