613fc74935e3e2ee9a4df9b086cd228b4addb1a4
aloha <wangfei8646@126.com>
2020-07-16 01:02:23 +0800

    7.16

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,4 +1,6 @@
-// pages/category/index.js
+
+import { request } from "../../request/index.js"
+
 Page({
 
   /**
@@ -6,61 +8,35 @@ Page({
    */
   data: {
 
+    first_cat: [],
+    second_cat: [],
+    third_cat: []
   },
 
+  Cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
+    this.getData()
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  getData() {
 
+    request({ url: "https://api-hmugo-web.itheima.net/api/public/v1/categories" })
+      .then(result => {
+
+        this.Cates = result.data.message
+        let first_cat = this.Cates.map(v =>v.cat_name)
+
+        let second_cat = this.Cates[0].children
+        this.setData({
+          first_cat,
+          second_cat
+        })
+
+      })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
